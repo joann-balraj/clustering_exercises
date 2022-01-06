bc2ec1903ba4af2bc158226d4a8bbf2cd63134a6
joann <joann.balraj@gmail.com>
2022-01-06 15:56:07 -0600

    updates

--- wrangle.py
+++ wrangle.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-
+import os
 import env
 
 
@@ -10,32 +10,32 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
 
 sql = """
-SELECT prop.*, 
-       pred.logerror, 
-       pred.transactiondate, 
-       air.airconditioningdesc, 
-       arch.architecturalstyledesc, 
-       build.buildingclassdesc, 
-       heat.heatingorsystemdesc, 
-       landuse.propertylandusedesc, 
-       story.storydesc, 
-       construct.typeconstructiondesc 
-FROM   properties_2017 prop  
+SELECT prop.*,
+       pred.logerror,
+       pred.transactiondate,
+       air.airconditioningdesc,
+       arch.architecturalstyledesc,
+       build.buildingclassdesc,
+       heat.heatingorsystemdesc,
+       landuse.propertylandusedesc,
+       story.storydesc,
+       construct.typeconstructiondesc
+FROM   properties_2017 prop
        INNER JOIN (SELECT parcelid,
-       					  logerror,
-                          Max(transactiondate) transactiondate 
-                   FROM   predictions_2017 
-                   GROUP  BY parcelid, logerror) pred
-               USING (parcelid) 
-       LEFT JOIN airconditioningtype air USING (airconditioningtypeid) 
-       LEFT JOIN architecturalstyletype arch USING (architecturalstyletypeid) 
-       LEFT JOIN buildingclasstype build USING (buildingclasstypeid) 
-       LEFT JOIN heatingorsystemtype heat USING (heatingorsystemtypeid) 
-       LEFT JOIN propertylandusetype landuse USING (propertylandusetypeid) 
-       LEFT JOIN storytype story USING (storytypeid) 
-       LEFT JOIN typeconstructiontype construct USING (typeconstructiontypeid) 
-WHERE  prop.latitude IS NOT NULL 
-       AND prop.longitude IS NOT NULL AND transactiondate <= '2017-12-31' 
+                   Max(transactiondate) transactiondate
+                   FROM   predictions_2017
+                   GROUP  BY parcelid) pred
+               USING (parcelid)
+               			JOIN predictions_2017 as pred USING (parcelid, transactiondate)
+       LEFT JOIN airconditioningtype air USING (airconditioningtypeid)
+       LEFT JOIN architecturalstyletype arch USING (architecturalstyletypeid)
+       LEFT JOIN buildingclasstype build USING (buildingclasstypeid)
+       LEFT JOIN heatingorsystemtype heat USING (heatingorsystemtypeid)
+       LEFT JOIN propertylandusetype landuse USING (propertylandusetypeid)
+       LEFT JOIN storytype story USING (storytypeid)
+       LEFT JOIN typeconstructiontype construct USING (typeconstructiontypeid)
+WHERE  prop.latitude IS NOT NULL
+       AND prop.longitude IS NOT NULL
 """
 
 def get_db_url(database):
@@ -55,52 +55,72 @@ def handle_missing_values(df, prop_required_column = .5, prop_required_row = .70
 	#function that will drop rows or columns based on the percent of values that are missing:\
 	#handle_missing_values(df, prop_required_column, prop_required_row
     threshold = int(round(prop_required_column*len(df.index),0))
-    df.dropna(axis=1, thresh=threshold, inplace=True)
+    df = df.dropna(axis=1, thresh=threshold)
     threshold = int(round(prop_required_row*len(df.columns),0))
     df.dropna(axis=0, thresh=threshold, inplace=True)
     return df
 
 
-def remove_columns(df, cols_to_remove):  
+def remove_columns(df, cols_to_remove):
 	#remove columns not needed
     df = df.drop(columns=cols_to_remove)
     return df
 
 def wrangle_zillow():
-    df = pd.read_csv('zillow.csv')
-    
-    # Restrict df to only properties that meet single unit use criteria
+    if os.path.isfile('zillow_cached.csv') == False:
+        df = get_zillow(sql)
+        df.to_csv('zillow_cached.csv',index = False)
+    else:
+        df = pd.read_csv('zillow_cached.csv')
+
+    # Restrict df to only properties that meet single use criteria
     single_use = [261, 262, 263, 264, 266, 268, 273, 276, 279]
     df = df[df.propertylandusetypeid.isin(single_use)]
-    
+
     # Restrict df to only those properties with at least 1 bath & bed and 350 sqft area
     df = df[(df.bedroomcnt > 0) & (df.bathroomcnt > 0) & ((df.unitcnt<=1)|df.unitcnt.isnull())\
             & (df.calculatedfinishedsquarefeet>350)]
 
     # Handle missing values i.e. drop columns and rows based on a threshold
     df = handle_missing_values(df)
-    
+
     # Add column for counties
-    df['county'] = np.where(df.fips == 6037, 'Los_Angeles',
-                           np.where(df.fips == 6059, 'Orange', 
-                                   'Ventura'))    
-    # drop columns not needed
-    df = remove_columns(df, ['id',
-       'calculatedbathnbr', 'finishedsquarefeet12', 'fullbathcnt', 'heatingorsystemtypeid'
-       ,'propertycountylandusecode', 'propertylandusetypeid','propertyzoningdesc', 
-        'censustractandblock', 'propertylandusedesc','heatingorsystemdesc','unitcnt'
-                            ,'buildingqualitytypeid'])
-    
+    df['county'] = df['fips'].apply(
+        lambda x: 'Los Angeles' if x == 6037\
+        else 'Orange' if x == 6059\
+        else 'Ventura')
+
+    # drop unnecessary columns
+    dropcols = ['parcelid',
+         'calculatedbathnbr',
+         'finishedsquarefeet12',
+         'fullbathcnt',
+         'heatingorsystemtypeid',
+         'propertycountylandusecode',
+         'propertylandusetypeid',
+         'propertyzoningdesc',
+         'censustractandblock',
+         'propertylandusedesc']
+
+    df = remove_columns(df, dropcols)
+
+    # replace nulls in unitcnt with 1
+    df.unitcnt.fillna(1, inplace = True)
+
+    # assume that since this is Southern CA, null means 'None' for heating system
+    df.heatingorsystemdesc.fillna('None', inplace = True)
+
     # replace nulls with median values for select columns
     df.lotsizesquarefeet.fillna(7313, inplace = True)
+    df.buildingqualitytypeid.fillna(6.0, inplace = True)
 
     # Columns to look for outliers
     df = df[df.taxvaluedollarcnt < 5_000_000]
-    df[df.calculatedfinishedsquarefeet < 8000]
-    
+    df = df[df.calculatedfinishedsquarefeet < 8000]
+
     # Just to be sure we caught all nulls, drop them here
     df = df.dropna()
-    
+
     return df
 
 def min_max_scaler(train, valid, test):
@@ -110,7 +130,7 @@ def min_max_scaler(train, valid, test):
     This is a linear transformation. Values will lie between 0 and 1
     '''
     num_vars = list(train.select_dtypes('number').columns)
-    scaler = MinMaxScaler(copy=True, feature_range=(0,1))
+    scaler = MinMaxScaler()
     train[num_vars] = scaler.fit_transform(train[num_vars])
     valid[num_vars] = scaler.transform(valid[num_vars])
     test[num_vars] = scaler.transform(test[num_vars])
@@ -123,35 +143,90 @@ def outlier_function(df, cols, k):
         q3 = df.annual_income.quantile(0.75)
         iqr = q3 - q1
         upper_bound =  q3 + k * iqr
-        lower_bound =  q1 - k * iqr     
+        lower_bound =  q1 - k * iqr
         df = df[(df[col] < upper_bound) & (df[col] > lower_bound)]
     return df
 
 def get_mall_customers(sql):
-	    url = get_db_url('mall_customers')
-	    mall_df = pd.read_sql(sql, url, index_col='customer_id')
-	    return mall_df
+    url = get_db_url('mall_customers')
+    mall_df = pd.read_sql(sql, url, index_col='customer_id')
+    return mall_df
+
+
+def train_validate_test_split(df):
+    train_and_validate, test = train_test_split(df, train_size=0.8, random_state=123)
+    train, validate = train_test_split(train_and_validate, train_size=0.75, random_state=123)
+    return train, validate, test
+
 
 def wrangle_mall_df():
-    
+
     # acquire data
     sql = 'select * from customers'
 
 
     # acquire data from SQL server
     mall_df = get_mall_customers(sql)
-    
+
     # handle outliers
     mall_df = outlier_function(mall_df, ['age', 'spending_score', 'annual_income'], 1.5)
-    
+
     # get dummy for gender column
     dummy_df = pd.get_dummies(mall_df.gender, drop_first=True)
     mall_df = pd.concat([mall_df, dummy_df], axis=1).drop(columns = ['gender'])
-    mall_df.rename(columns= {'Male': 'is_male'}, inplace = True)
-    # return mall_df
 
-    # split the data in train, validate and test
-    train, test = train_test_split(mall_df, train_size = 0.8, random_state = 123)
-    train, validate = train_test_split(train, train_size = 0.75, random_state = 123)
-    
-    return min_max_scaler, train, validate, test
+    train, validate, test = train_validate_test_split(mall_df)
+
+    return min_max_scaler(train, validate, test)
+
+def nulls_by_col(df):
+    num_missing = df.isnull().sum()
+    rows = df.shape[0]
+    prcnt_miss = num_missing / rows * 100
+    cols_missing = pd.DataFrame({'num_rows_missing': num_missing, 'percent_rows_missing': prcnt_miss})
+    return cols_missing
+
+def nulls_by_row(df):
+    num_missing = df.isnull().sum(axis=1)
+    prcnt_miss = num_missing / df.shape[1] * 100
+    rows_missing = pd.DataFrame({'num_cols_missing': num_missing, 'percent_cols_missing': prcnt_miss})\
+    .reset_index()\
+    .groupby(['num_cols_missing', 'percent_cols_missing']).count()\
+    .rename(index=str, columns={'customer_id': 'num_rows'}).reset_index()
+    return rows_missing
+
+def summarize(df):
+    '''
+    summarize will take in a single argument (a pandas dataframe)
+    and output to console various statistics on said dataframe, including:
+    # .head()
+    # .info()
+    # .describe()
+    # value_counts()
+    # observation of nulls in the dataframe
+    '''
+    print('=====================================================\n\n')
+    print('Dataframe head: ')
+    print(df.head(3).to_markdown())
+    print('=====================================================\n\n')
+    print('Dataframe info: ')
+    print(df.info())
+    print('=====================================================\n\n')
+    print('Dataframe Description: ')
+    print(df.describe().to_markdown())
+    num_cols = [col for col in df.columns if df[col].dtype != 'O']
+    cat_cols = [col for col in df.columns if col not in num_cols]
+    print('=====================================================')
+    print('DataFrame value counts: ')
+    for col in df.columns:
+        if col in cat_cols:
+            print(df[col].value_counts())
+        else:
+            print(df[col].value_counts(bins=10, sort=False))
+    print('=====================================================')
+    print('nulls in dataframe by column: ')
+    print(nulls_by_col(df))
+    print('=====================================================')
+    print('nulls in dataframe by row: ')
+    print(nulls_by_row(df))
+    print('============================================')
